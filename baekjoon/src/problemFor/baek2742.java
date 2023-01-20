f94466fe3bb590af391f3a356a060babddb8155b
parkjeehoon <gkstj8300@naver.com>
2023-01-20 21:58:26 +0900

    2023.01.20_백준문제풀이

--- baekjoon/src/problemFor/baek2742.java
+++ baekjoon/src/problemFor/baek2742.java
@@ -7,23 +7,23 @@ import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class baek2742 {
-
-	public static void main(String [] args) throws Exception {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine().trim());
-		
-		for(int i = n; i >= 1; i--) {
-			bw.write(i + "\n");
-		}
-		
-		bw.flush();
-		bw.close();
-	}
+ 
+//	public static void main(String [] args) throws Exception {
+//		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		
+//		int n = Integer.parseInt(br.readLine().trim());
+//		
+//		for(int i = n; i >= 1; i--) {
+//			bw.write(i + "\n");
+//		}
+//		bw.flush();
+//		bw.close();
+//		
+//	}
 	
-	/*public static void main(String [] args) throws Exception {
+	public static void main(String [] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = 5;
@@ -32,6 +32,6 @@ public class baek2742 {
 			System.out.println(i);
 		}
 		
-	}*/
+	}
 	
 }
---
+++ baekjoon/src/problemFor/baek3449.java
@@ -0,0 +1,16 @@
+package problemFor;
+
+import java.util.Scanner;
+
+public class baek3449 {
+	public static void main(String [] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 5;
+		
+		for(int i = n; i >= 1; i--) {
+			System.out.println(i);
+		}
+		
+	}
+}
