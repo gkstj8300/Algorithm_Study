3c921bc22a9029857bfd955a18c7853e54a3a3b9
ParkJeeHoon <gkstj8300@naver.com>
2023-04-18 16:48:15 +0900

    2023.04.18_ 백준 restart

--- baekjoon/src/problemFor/baek2742.java
+++ baekjoon/src/problemFor/baek2742.java
@@ -23,15 +23,15 @@ public class baek2742 {
 		
 	}
 	
-//	public static void main(String [] args) throws Exception {
-//		Scanner sc = new Scanner(System.in);
-//		
-//		int n = 5;
-//		
-//		for(int i = n; i >= 1; i--) {
-//			System.out.println(i);
-//		}
-//		
-//	}
+	public static void main(String [] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 5;
+		
+		for(int i = n; i >= 1; i--) {
+			System.out.println(i);
+		}
+		
+	}
 	
 }
