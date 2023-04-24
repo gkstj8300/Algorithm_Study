c531770fa1da765d3275d2c231b99b79db03986e
parkjeehoon <gkstj8300@naver.com>
2023-04-24 15:09:23 +0900

    2023.04.24_백준 restart

--- baekjoon/src/problemFor/baek11021.java
+++ baekjoon/src/problemFor/baek11021.java
@@ -11,22 +11,22 @@ public class baek11021 {
 
 	public static void main(String[] args) throws Exception {
 		
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int t = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= t; i++) {
-            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken());
-            int b = Integer.parseInt(tokenizer.nextToken());
-            bw.write("Case #" + i + ": " + (a + b) + "\n");
-        }
-		br.close(); 
-		bw.flush();
-		bw.close();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int t = Integer.parseInt(br.readLine());
+//
+//        for (int i = 1; i <= t; i++) {
+//            StringTokenizer tokenizer = new StringTokenizer(br.readLine());
+//            int a = Integer.parseInt(tokenizer.nextToken());
+//            int b = Integer.parseInt(tokenizer.nextToken());
+//            bw.write("Case #" + i + ": " + (a + b) + "\n");
+//        }
+//		br.close(); 
+//		bw.flush();
+//		bw.close();
 		
-		/*Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int t = sc.nextInt();
 
@@ -35,7 +35,7 @@ public class baek11021 {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			System.out.println("Case #"+i+": "+(a+b));
-		}*/
+		}
 		
 		
 	}
