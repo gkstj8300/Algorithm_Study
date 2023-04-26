86c586465e1500154a2f3d43eae9c9fe03aa6b48
ParkJeeHoon <gkstj8300@naver.com>
2023-04-26 09:21:26 +0900

    2023.04.26_백준 restart

--- baekjoon/src/problemArray/baek10818.java
+++ baekjoon/src/problemArray/baek10818.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 public class baek10818 {
 
     public static void main(String[] args){
-//    	Scanner sc = new Scanner(System.in);
-//    	
-//    	int n = sc.nextInt();
-//    	int[] arr = new int[n];
-//
-//		
-//		for(int i = 0; i < n; i++) {
-//			arr[i] = sc.nextInt();
-//		}
-//		int max = arr[0];
-//		int min = arr[0];
-//		
-//    	for(int i = 0; i < n; i++) {
-//    	
-//    		if(arr[i] > max) {
-//    			max = arr[i];
-//    		}else if(arr[i] < min) {
-//    			min = arr[i];
-//    		}
-//    	}
-//    	System.out.println(min+" "+max);
+    	Scanner sc = new Scanner(System.in);
+    	
+    	int n = sc.nextInt();
+    	int[] arr = new int[n];
+
+		
+		for(int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max = arr[0];
+		int min = arr[0];
+		
+    	for(int i = 0; i < n; i++) {
+    	
+    		if(arr[i] > max) {
+    			max = arr[i];
+    		}else if(arr[i] < min) {
+    			min = arr[i];
+    		}
+    	}
+    	System.out.println(min+" "+max);
         
         
 
