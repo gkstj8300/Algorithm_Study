e506aaa847e718f8ead4cbf3696df60223231481
ParkJeeHoon <gkstj8300@naver.com>
2023-05-18 13:05:30 +0900

    2023.05.18_백준 restart

--- baekjoon/src/problemWhile/baek10952.java
+++ baekjoon/src/problemWhile/baek10952.java
@@ -30,30 +30,30 @@ public class baek10952 {
 		
 		
 		
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		
-//		StringBuilder sb = new StringBuilder();
-//		
-//		while(true) {
-//			
-//			String str = br.readLine();
-//			StringTokenizer st = new StringTokenizer(str, " ");
-//			int a = Integer.parseInt(st.nextToken());
-//			int b = Integer.parseInt(st.nextToken());
-//
-//			
-//			if(a == 0 && b == 0) {
-//				break;
-//			}
-//			
-//			sb.append((a+b)+"\n");
-//		}
-//		System.out.println(sb);
-//		
-//		br.close();
-//		bw.flush();
-//		bw.close();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str, " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			
+			if(a == 0 && b == 0) {
+				break;
+			}
+			
+			sb.append((a+b)+"\n");
+		}
+		System.out.println(sb);
+		
+		br.close();
+		bw.flush();
+		bw.close();
 		 
 	}
 
