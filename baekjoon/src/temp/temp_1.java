a258b94ec86764331c27cc8cbee2ffba8d89644c
parkjeehoon <gkstj8300@naver.com>
2023-06-19 20:21:27 +0900

    2023.06.19_백준 test

--- baekjoon/src/temp/temp_1.java
+++ baekjoon/src/temp/temp_1.java
@@ -26,15 +26,15 @@ public class temp_1 {
 		bw.flush();
 		bw.close();
 		
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		
-		for(int i = 1; i <= t; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
-		}
-		
-		sc.close();
+//		Scanner sc = new Scanner(System.in);
+//		int t = sc.nextInt();
+//		
+//		for(int i = 1; i <= t; i++) {
+//			int a = sc.nextInt();
+//			int b = sc.nextInt();
+//			System.out.println("Case #"+i+": "+a+" + "+b+" = "+(a+b));
+//		}
+//		
+//		sc.close();
 	}
 }
