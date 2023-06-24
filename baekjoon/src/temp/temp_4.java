dbc80c90a49839c828d1e24f3c6c339e93f96f35
parkjeehoon <gkstj8300@naver.com>
2023-06-24 11:06:03 +0900

    2023.06.24_백준 start

---
+++ baekjoon/src/temp/temp_4.java
@@ -0,0 +1,33 @@
+package temp;
+
+import java.util.Scanner;
+
+public class temp_4 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next().toUpperCase();
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < s.length(); i++) {
+			int a = s.charAt(i) - 'A';
+			arr[a]++;
+		}
+		
+		int max = 0;
+		char c = '?';
+		for(int i = 0; i < arr.length; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+				c = (char)(i+'A');
+			}else if (max == arr[i]) {
+				c = '?';
+			}
+		}
+		System.out.println(c);
+
+	}
+
+}
