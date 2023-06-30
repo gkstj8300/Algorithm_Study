88084434d9701b9ebfc36cb4b7aee6987bb665cb
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-06-30 13:30:37 +0900

    2023.06.30_백준 restart

--- baekjoon/src/temp/temp_7.java
+++ baekjoon/src/temp/temp_7.java
@@ -12,17 +12,17 @@ public class temp_7 {
 		StringTokenizer st = new StringTokenizer(str, " ");
 		System.out.println(st.countTokens());
 		
-//		//2踰덉㎏ ���씠�뒗 ��由�
-//		Scanner sc = new Scanner(System.in);
-//		String arr = sc.nextLine();
-//		int count = 0;
-//		
-//		for(int i = 0; i < arr.length() - 1; i++) {
-//			if(arr.charAt(i) == 32) {
-//				count++;
-//			}
-//		}
-//		System.out.println(count);
+		//2踰덉㎏ ���씠�뒗 ��由�
+		Scanner sc = new Scanner(System.in);
+		String arr = sc.nextLine();
+		int count = 0;
+		
+		for(int i = 0; i < arr.length() - 1; i++) {
+			if(arr.charAt(i) == 32) {
+				count++;
+			}
+		}
+		System.out.println(count);
 		
 		//
 //		Scanner sc = new Scanner(System.in);
