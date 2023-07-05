fd45cd816ac2ef58d76dba248ef2d67c2fa14742
parkjeehoon <gkstj8300@naver.com>
2023-07-06 00:00:45 +0900

    2023.07.06_백준 restart

---
+++ baekjoon/src/temp/temp_9.java
@@ -0,0 +1,31 @@
+package temp;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class temp_9 {
+	public static void main(String[] args) throws IOException{
+
+		
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int a = Integer.parseInt(br.readLine().trim());
+		
+		for(int i = 0; i < a; i++) {
+			String text = br.readLine();
+			String[] word = text.split(" ");
+			int b = Integer.parseInt(word[0]);
+			int c = Integer.parseInt(word[1]);
+			bw.write((b+c) + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		
+		
+	}
+}
