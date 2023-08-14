de49098e7f11dc6e7af376661568a8dd5726bfe2
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-08-14 16:53:17 +0900

    2023.08.14_백준 restart

--- baekjoon/src/temp/temp_5.java
+++ baekjoon/src/temp/temp_5.java
@@ -5,50 +5,50 @@ import java.util.Scanner;
 public class temp_5 {
 	
 	public static void main() {
-//		Scanner sc = new Scanner(System.in);
-//		
-//
-//		
-//		while(true) {
-//			
-//			int a = sc.nextInt();
-//			int b = sc.nextInt();
-//			
-//			if(a==0 && b==0) {
-//				break;
-//			}
-//			
-//			System.out.println(a+b);
-//			
-//		}
-//		sc.close();
+		Scanner sc = new Scanner(System.in);
 		
-		
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringBuilder sb = new StringBuilder();
+
 		
 		while(true) {
 			
-			String str = br.readLine();
-			StringTokenizer st = new StringTokenizer(str, " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-
+			int a = sc.nextInt();
+			int b = sc.nextInt();
 			
-			if(a == 0 && b == 0) {
+			if(a==0 && b==0) {
 				break;
 			}
 			
-			sb.append((a+b)+"\n");
+			System.out.println(a+b);
+			
 		}
-		System.out.println(sb);
+		sc.close();
+		 
 		
-		br.close();
-		bw.flush();
-		bw.close();
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		
+//		StringBuilder sb = new StringBuilder();
+//		
+//		while(true) {
+//			
+//			String str = br.readLine();
+//			StringTokenizer st = new StringTokenizer(str, " ");
+//			int a = Integer.parseInt(st.nextToken());
+//			int b = Integer.parseInt(st.nextToken());
+//
+//			
+//			if(a == 0 && b == 0) {
+//				break;
+//			}
+//			
+//			sb.append((a+b)+"\n");
+//		}
+//		System.out.println(sb);
+		
+//		br.close();
+//		bw.flush();
+//		bw.close();
 	}
 	
 }
