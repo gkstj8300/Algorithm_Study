9dbeaa3a019fbd6c87e1e52ebb67bcf7865441a1
parkjeehoon <gkstj8300@naver.com>
2023-08-26 02:03:54 +0900

    2023.08.26_백준 restart

--- baekjoon/src/temp/temp_15.java
+++ baekjoon/src/temp/temp_15.java
@@ -12,7 +12,7 @@ public class temp_15 {
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = sc.next();
 		}		
-		for(int i = 0; i < arr.length; i++) {
+		for(int i = 0; i < arr.length; i++) { 
 			int count = 0;
 			int sum = 0;
 			
