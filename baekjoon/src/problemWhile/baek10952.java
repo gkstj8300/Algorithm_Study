e2993af007c104d82df9f180e35688c44ec92e13
parkjeehoon <gkstj8300@naver.com>
2023-08-31 23:46:32 +0900

    2023.08.31_백준 restart

--- baekjoon/src/problemWhile/baek10952.java
+++ baekjoon/src/problemWhile/baek10952.java
@@ -10,23 +10,23 @@ public class baek10952 {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		
-
-		
-		while(true) {
-			
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			
-			if(a==0 && b==0) {
-				break;
-			}
-			
-			System.out.println(a+b);
-			
-		}
-		sc.close();
+//		Scanner sc = new Scanner(System.in);
+//		
+//
+//		
+//		while(true) {
+//			
+//			int a = sc.nextInt();
+//			int b = sc.nextInt();
+//			
+//			if(a==0 && b==0) {
+//				break;
+//			}
+//			
+//			System.out.println(a+b);
+//			
+//		}
+//		sc.close();
 		
 		
 		
