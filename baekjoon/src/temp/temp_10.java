46c42a25a324fd9da1ef232459ae5a90e16b0dd6
parkjeehoon <gkstj8300@naver.com>
2023-09-25 20:57:51 +0900

    2023.09.25_백준 restart

--- baekjoon/src/temp/temp_10.java
+++ baekjoon/src/temp/temp_10.java
@@ -29,26 +29,6 @@ public class temp_10 {
 			System.out.println("%");
 			
 		}		
-		for(int i = 0; i < n; i++) {
-			int[] count = new int[sc.nextInt()];
-			int total = 0; 
-			int high = 0;
-			
-			for(int j = 0; j < count.length; j++) {
-				count[j] = sc.nextInt();
-				total += count[j];
-			} 
-			 
-			double avg = (double)total / count.length;
-			
-			for(int j = 0; j < count.length; j++) {
-				if(count[j] > avg) {
-					high++;
-				}
-			}
-			System.out.printf("%.3f", 100.0 * high / count.length);
-			System.out.println("%");
-			
-		}	
+	
 	}
 }
