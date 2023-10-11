477025b5caaad47ba0b74656f25cbcd880f142a8
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-10-11 17:05:54 +0900

    2023.10.11_백준 temp_7 2번째 풀이

--- src/baekjoon/temp/temp_7.java
+++ src/baekjoon/temp/temp_7.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 public class temp_7 {
 	public static void main(String[] args) {
 		//
-		Scanner sc = new Scanner(System.in);
-		
-		String str = sc.nextLine();
-		
-
-		String arr = sc.nextLine();
-		int count = 0;
-		
-		for(int i = 0; i < arr.length() - 1; i++) {
-			if(arr.charAt(i) == 32) {
-				count++;
-			}
-		}
-
-		
-//		
 //		Scanner sc = new Scanner(System.in);
 //		
-//		String st = sc.nextLine().trim();
-//		sc.close();
+//		String str = sc.nextLine();
 //		
-//		if(st.isEmpty()) {
-//			System.out.println(0);
-//		}else {
-//			System.out.println(st.split(" ").length);
+//
+//		String arr = sc.nextLine();
+//		int count = 0;
+//		
+//		for(int i = 0; i < arr.length() - 1; i++) {
+//			if(arr.charAt(i) == 32) {
+//				count++;
+//			}
 //		}
+
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String st = sc.nextLine().trim();
+		sc.close();
+		
+		if(st.isEmpty()) {
+			System.out.println(0);
+		}else {
+			System.out.println(st.split(" ").length);
+		}
 		
 	}
 }
