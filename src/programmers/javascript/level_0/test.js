7a4179269c8b4ef433ef433844ad94a62f2ac3e0
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-10-17 17:50:27 +0900

    2023.10.17_프로그래머스 level_0 javascript 대소문자 바꿔서 출력하기 진행중

--- src/programmers/javascript/level_0/test.js
+++ src/programmers/javascript/level_0/test.js
@@ -2,24 +2,32 @@ const fs = require('fs');
 const input = fs.readFileSync('c:/Users/user/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().split(' ');
 const input2 = fs.readFileSync('c:/Users/user/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().trim(' ');
 
-let str = "";
-const num = Number(input[1]);
-for(let i = 0; i < num; i++){
-    str += input[0];
-};
-console.log(str);
+let arr = input[0].split("");
+// arr.forEach((str) => console.log(str));
 
+arr.forEach(function (str) { 
+    console.log(str);
+    console.log(str.toLowerCase);
+});
+
+function isUpper(str){
+    str.toUpperCase();
+    return str;
+}
+function isLower(){
+
+}
 
 // * 문제 설명
-// 문자열 str과 정수 n이 주어집니다.
-// str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+// 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 
+// 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
 
 // * 제한사항
-// 1 ≤ str의 길이 ≤ 10
-// 1 ≤ n ≤ 5
+// 1 ≤ str의 길이 ≤ 20
+// str은 알파벳으로 이루어진 문자열입니다.
 
 // * 입출력 예
 // 입력 #1
-// string 5
+// aBcDeFg
 // 출력 #1
-// stringstringstringstringstring
+// AbCdEfG
