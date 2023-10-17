0a711f3af8bab3d632c56a8fee851ea155b289cb
[NDCC] Park jee-hoon <jh.m02h.park@misumi.co.kr>
2023-10-17 16:04:01 +0900

    2023.10.17_프로그래머스 level_0 javascript 문자열 반복해서 출력하기 완료

--- src/programmers/javascript/level_0/test.js
+++ src/programmers/javascript/level_0/test.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
-const input1 = fs.readFileSync('c:/Users/user/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().split(' ');
+const input = fs.readFileSync('c:/Users/user/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().split(' ');
 const input2 = fs.readFileSync('c:/Users/user/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().trim(' ');
 
-console.log(`a = ${Number(input1[0])}\nb = ${Number(input1[1])}`);
-console.log(Number(input1[0])+Number(input1[1]));
-// console.log(input2);
+let str = "";
+const num = Number(input[1]);
+for(let i = 0; i < num; i++){
+    str += input[0];
+};
+console.log(str);
+
+
+// * 문제 설명
+// 문자열 str과 정수 n이 주어집니다.
+// str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+
+// * 제한사항
+// 1 ≤ str의 길이 ≤ 10
+// 1 ≤ n ≤ 5
+
+// * 입출력 예
+// 입력 #1
+// string 5
+// 출력 #1
+// stringstringstringstringstring
