a503dbe391ec431bcd282f2f729fe1664f39b5b0
parkjeehoon <gkstj8300@naver.com>
2023-10-17 23:01:10 +0900

    2023.10.17_프로그래머스 level_0 javascript 홀짝 구분하기 완료

--- src/programmers/javascript/level_0/test.js
+++ src/programmers/javascript/level_0/test.js
@@ -2,23 +2,29 @@ const fs = require('fs');
 const input = fs.readFileSync('C:/Users/gkstj/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().split(' ');
 // const input2 = fs.readFileSync('/src/programmers/javascript/level_0/input.txt').toString().trim(' ');
 
-let strarr = input[0].split("");
-strarr.forEach((str) => console.log(str));
+let num = input[0];
+num = (num%2==0) ? num+" is even" : num+" is odd";
+console.log(num);
+
 
 
 /*
-      덧셈식 출력하기
+      홀짝 구분하기
 
       * 문제 설명
-      두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
-      a + b = c
+      자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 
+      홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
 
       * 제한사항
-      1 ≤ a, b ≤ 100
+      1 ≤ n ≤ 1,000
 
       * 입출력 예
       입력 #1
-      4 5
+      100
       출력 #1
-      4 + 5 = 9
+      100 is even
+      입력 #2
+      1
+      출력 #2
+      1 is odd
 */
