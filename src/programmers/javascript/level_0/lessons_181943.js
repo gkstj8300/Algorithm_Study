faf7132170864657364e30cdbcd92099b91d6a38
parkjeehoon <gkstj8300@naver.com>
2023-10-17 23:18:14 +0900

    2023.10.17_프로그래머스 level_0 javascript 문자열 겹쳐쓰기 완료

--- src/programmers/javascript/level_0/lessons_181943.js
+++ src/programmers/javascript/level_0/lessons_181943.js
@@ -1,4 +1,6 @@
 /*
+      문자열 겹쳐쓰기
+      
       * 문제 설명
       문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
       문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 
@@ -14,3 +16,8 @@
       "He11oWor1d"	"lloWorl"	2	"HelloWorld"
       "Program29b8UYP"	"merS123"	7	"ProgrammerS123"
  */
+
+
+function solution(my_string, overwrite_string, s) {
+      return my_string.replace(my_string.substring(s, overwrite_string.length+s),overwrite_string);
+}
--- src/programmers/javascript/level_0/test.js
+++ src/programmers/javascript/level_0/test.js
@@ -2,28 +2,27 @@ const fs = require('fs');
 const input = fs.readFileSync('C:/Users/gkstj/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().split(' ');
 // const input2 = fs.readFileSync('/src/programmers/javascript/level_0/input.txt').toString().trim(' ');
 
-let data = input[0];
-console.log(input);
+let str = solution("He11oWor1d","lloWorl", 2);
+let str2 = solution("Program29b8UYP","merS123",7);
+console.log(str2);
 
-
+function solution(my_string, overwrite_string, s) {
+    return my_string.replace(my_string.substring(s, overwrite_string.length+s),overwrite_string);
+}
 
 /*
-      홀짝 구분하기
-
       * 문제 설명
-      자연수 n이 입력으로 주어졌을 때 만약 n이 짝수이면 "n is even"을, 
-      홀수이면 "n is odd"를 출력하는 코드를 작성해 보세요.
+      문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
+      문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 
+      문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
 
       * 제한사항
-      1 ≤ n ≤ 1,000
+      my_string와 overwrite_string은 숫자와 알파벳으로 이루어져 있습니다.
+      1 ≤ overwrite_string의 길이 ≤ my_string의 길이 ≤ 1,000
+      0 ≤ s ≤ my_string의 길이 - overwrite_string의 길이
 
       * 입출력 예
-      입력 #1
-      100
-      출력 #1
-      100 is even
-      입력 #2
-      1
-      출력 #2
-      1 is odd
-*/
+      my_string	overwrite_string	s	result
+      "He11oWor1d"	"lloWorl"	2	"HelloWorld"
+      "Program29b8UYP"	"merS123"	7	"ProgrammerS123"
+ */
