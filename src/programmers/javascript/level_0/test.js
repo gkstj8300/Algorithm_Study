bbe9f591a6cac9264631d4e0e2e58979feb20f74
parkjeehoon <gkstj8300@naver.com>
2023-10-17 22:22:33 +0900

    2023.10.17_프로그래머스 level_0 javascript 덧셈식 출력하기 완료

--- src/programmers/javascript/level_0/test.js
+++ src/programmers/javascript/level_0/test.js
@@ -2,28 +2,24 @@ const fs = require('fs');
 const input = fs.readFileSync('C:/Users/gkstj/git/Algorithm_Study/src/programmers/javascript/level_0/input.txt').toString().split(' ');
 // const input2 = fs.readFileSync('/src/programmers/javascript/level_0/input.txt').toString().trim(' ');
 
-let arr = input[0].split("");
-let data = "";
-arr.forEach(function (str) { 
-    if(str === str.toUpperCase()){
-        data += str.toLowerCase();
-    }else if(str === str.toLowerCase()){
-        data += str.toUpperCase();
-    }
-});
-console.log('!@#$%^&*(\\\'"<>?:;'); 
-console.log('!@#$%^&*(\\\'"<>?:;');
+let num1 = input[0];
+let num2 = input[1];
+console.log(Number(num1)+" + "+Number(num2)+" = "+(Number(num1)+Number(num2)));
 
-// * 문제 설명
-// 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 
-// 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
 
-// * 제한사항
-// 1 ≤ str의 길이 ≤ 20
-// str은 알파벳으로 이루어진 문자열입니다.
+/*
+      덧셈식 출력하기
 
-// * 입출력 예
-// 입력 #1
-// aBcDeFg
-// 출력 #1
-// AbCdEfG
+      * 문제 설명
+      두 정수 a, b가 주어질 때 다음과 같은 형태의 계산식을 출력하는 코드를 작성해 보세요.
+      a + b = c
+
+      * 제한사항
+      1 ≤ a, b ≤ 100
+
+      * 입출력 예
+      입력 #1
+      4 5
+      출력 #1
+      4 + 5 = 9
+*/
