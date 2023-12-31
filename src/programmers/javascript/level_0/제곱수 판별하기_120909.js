aad63aec26a8a0ee608b106f4c3d9dba933681af
parkjeehoon <gkstj8300@naver.com>
2023-12-31 23:57:30 +0900

    2023.12.31_[Level0]_제곱수 판별하기 완료

---
+++ src/programmers/javascript/level_0/제곱수 판별하기_120909.js
@@ -0,0 +1,11 @@
+console.log( solution(10) )
+
+function solution(n) {
+    let arr = [];
+    for(let i = 1; i <= n; i++) {
+        if(n % i === 0) {
+            arr.push(n / i);
+        }
+    }
+    return arr.length % 2 ? 1 : 2;
+}
